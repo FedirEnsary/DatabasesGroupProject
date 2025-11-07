4c8008de278a7c8bb6c6200e336f59090f49e256
Fedir Ensary <fedir.ensary@gmail.com>
2025-11-07 12:49:32 -0600

    added requirements.txt

--- importdata.py
+++ importdata.py
@@ -6,10 +6,17 @@ load_dotenv()
 
 conn = get_db_connection()
 
-file_path = 'imdb_top_1000.csv'
+file_path = 'imdb-movies-dataset.csv'
 
 with open(file_path, 'r', newline='', encoding="utf8") as csvfile:
     csvreader = csv.reader(csvfile)
 
     for i, r in enumerate(csvreader):
-        print(r)
+        for t in r:
+            print(r)
+        name = r[1]
+        desc = r[7]
+        year = r[2]
+        rtng = r[6]
+        crtc = r[8]
+        gnre = r[4]
